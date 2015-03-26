2ab97dfd2610e3bf58ae70da90899b8089c1be3f
Steve <swoodis2010@gmail.com>
2015-03-26 07:25:35 -0600

    More css. Fixed submitCtrl file and updated stylesheet.

--- aboutmeCtrl.js
+++ aboutmeCtrl.js
@@ -1,13 +1,11 @@
 var app = angular.module('drawWorksApp');
-  app.controller('aboutMeCtrl',function($scope, $q, envService){
-
+  app.controller('aboutMeCtrl',function($scope, $q, $firebaseObject, envService){
       var User = envService.getUser(); 
       User.$loaded(function(data) {
       });
       $scope.User = User;
-      
       $scope.updateMe = function(){
-       console.log('you made it to the Save function ');   
+           $scope.User.$save()
       }
      
 })
